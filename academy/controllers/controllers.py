5cc7830c710a8a55d5b3c317eb6ca6e825196a4b
adabbasi <adabbasi@gmail.com>
2022-11-04 06:15:44 +0000

    Updated controllers.py & templates.xml

--- academy/controllers/controllers.py
+++ academy/controllers/controllers.py
@@ -2,12 +2,12 @@
 from odoo import http
 
 
-class Academy(http.Controller):
+ class Academy(http.Controller):
     
        @http.route('/academy/<model("academy.teachers"):teacher>/', auth='public', website=True)
-       def teacher(self, teacher):
-        return http.request.render('academy.biography', {
-        'person': teacher
+        def teacher(self, teacher):
+           return http.request.render('academy.biography', {
+             'person': teacher
     })
 
 #     @http.route('/academy/academy/', auth='public', website=True)
